37c94aba05727352fc704b6da35a892f7dcf2b5b
nguyen minh anh <nanh2832003@gmail.com>
2024-12-27 09:51:46 +0700

    Them model, controller, service cua recipingredient

---
+++ demoDataFirst/Services/IRecipeIngredientService.cs
@@ -0,0 +1,13 @@
+﻿using demoDataFirst.Models;
+
+namespace demoDataFirst.Services
+{
+    public interface IRecipeIngredientService
+    {
+        IEnumerable<RecipeIngredient> GetAll();
+        RecipeIngredient GetById(int id);
+        void Add(RecipeIngredient recipeIngredient);
+        void Update(RecipeIngredient recipeIngredient);
+        void Delete(int id);
+    }
+}
---
+++ demoDataFirst/Services/RecipeIngredientService.cs
@@ -0,0 +1,43 @@
+﻿using demoDataFirst.Models;
+using demoDataFirst.Repositories;
+
+namespace demoDataFirst.Services
+{
+    public class RecipeIngredientService : IRecipeIngredientService
+    {
+        private readonly IGenericRepository<RecipeIngredient> _repository;
+
+        public RecipeIngredientService(IGenericRepository<RecipeIngredient> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<RecipeIngredient> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public RecipeIngredient GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void Add(RecipeIngredient recipeIngredient)
+        {
+            _repository.Add(recipeIngredient);
+            _repository.Save();
+        }
+
+        public void Update(RecipeIngredient recipeIngredient)
+        {
+            _repository.Update(recipeIngredient);
+            _repository.Save();
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+            _repository.Save();
+        }
+    }
+}
