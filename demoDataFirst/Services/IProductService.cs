a1aa6524e13fca90e8e323ee98c089a561e19476
nguyen minh anh <nanh2832003@gmail.com>
2025-01-01 22:12:37 +0700

    thêm annotation cho model product và tạo controller, service

---
+++ demoDataFirst/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using demoDataFirst.Models;
+
+namespace demoDataFirst.Services
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        Task CreateProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
