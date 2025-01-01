f4a3c035fb18aa2a46259fdf34c93f3b5621f1a9
nguyen minh anh <nanh2832003@gmail.com>
2025-01-01 21:30:16 +0700

    them service va controller cho order

---
+++ demoDataFirst/Services/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using demoDataFirst.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace demoDataFirst.Services
+{
+    public interface IOrderService
+    {
+        IEnumerable<Order> GetAllOrders();
+        Order GetOrderById(int id);
+        Task CreateAsync(Order order);
+        Task UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(int id);
+    }
+}
---
+++ demoDataFirst/Services/OrderService .cs
@@ -0,0 +1,52 @@
+﻿using demoDataFirst.Models;
+using demoDataFirst.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace demoDataFirst.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IGenericRepository<Order> _orderRepository;
+
+        public OrderService(IGenericRepository<Order> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return _orderRepository.GetAll();
+        }
+
+        public Order GetOrderById(int id)
+        {
+            return _orderRepository.GetById(id);
+        }
+
+        public async Task CreateAsync(Order order)
+        {
+            // Kiểm tra nếu Order đã tồn tại (nếu có điều kiện cụ thể)
+            var existingOrder = await _orderRepository.GetByConditionAsync(o => o.OrderId == order.OrderId);
+            if (existingOrder != null)
+            {
+                throw new Exception("Order already exists.");
+            }
+
+            await _orderRepository.AddAsync(order);
+            await _orderRepository.SaveAsync();
+        }
+
+        public async Task UpdateOrderAsync(Order order)
+        {
+            _orderRepository.UpdateAsync(order);
+            await _orderRepository.SaveAsync();
+        }
+
+        public async Task DeleteOrderAsync(int id)
+        {
+            await _orderRepository.DeleteAsync(id);
+        }
+    }
+}
